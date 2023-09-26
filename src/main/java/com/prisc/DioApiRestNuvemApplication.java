be5e81499596642d67d0521923e8b52bfe3b0b17
Priscila <priscila.campoos@yahoo.com.br>
2023-09-26 18:18:00 +0100

    Projeto

---
+++ src/main/java/com/prisc/DioApiRestNuvemApplication.java
@@ -0,0 +1,13 @@
+package com.prisc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioApiRestNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioApiRestNuvemApplication.class, args);
+	}
+
+}
